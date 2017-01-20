74d32e9cb6adaec02de1a7e1cacff3af2abf68ad
nishantdania <nishantdania@gmail.com>
2017-01-20 22:52:01 +0530

    added node server to serve pages

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express()
+
+app.use(express.static(__dirname));
+
+app.get('/coupons', function (req, res) {
+  res.sendFile(__dirname + '/coupons.html');
+})
+
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000!')
+})
